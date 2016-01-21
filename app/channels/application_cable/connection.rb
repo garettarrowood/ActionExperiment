89dcbf90c7c35bd333c1fa0b0940975a920ee29d
Garett Arrowood <garettarrowood@gmail.com>
2016-01-21 12:32:59 -0500

    Add username into rendered partial

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,11 +1,11 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in an EventMachine loop that does not support auto reloading.
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
-    # identified_by :current_user
+    identified_by :current_user
 
-    # def connect
-    #   self.current_user = find_verified_user
-    # end
+    def connect
+      self.current_user = cookies.signed[:username]
+    end
 
     # protected
 
